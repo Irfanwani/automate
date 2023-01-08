c85bbac59ddb9b899532402839e30a78e1aded42
Irfanwani <irfanwani347@gmail.com>
2023-01-08 22:13:21 +0530

    whatsapp automation

---
+++ whatsapp.py
@@ -0,0 +1,10 @@
+from pywhatkit import sendwhatmsg_instantly
+
+
+def main():
+    sendwhatmsg_instantly("+91xxxxxxxxxx", 'Hello (Automated by python)')
+
+
+
+if __name__ == '__main__':
+    main()
